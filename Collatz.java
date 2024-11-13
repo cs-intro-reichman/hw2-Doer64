d24fc84a94c4e1b5276150add8d40beec910c415
Dean Orion <60265116+Doer64@users.noreply.github.com>
2024-11-13 17:45:33 +0200

    Coded everything

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,37 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int upTo = Integer.parseInt(args[0]);
+		boolean verbose = args[1].equals("v");
+
+		for(int i = 1; i <= upTo; i++){
+			int count = 2;
+			int num = i;
+			if (verbose)  //print the first number
+					System.out.print(num + " ");
+
+			if(num % 2 == 0) //runs for the first time in case upTo = 1
+				num = num/2;
+			else
+				num = num*3 + 1;
+			if (verbose) 
+				System.out.print(num + " ");
+
+			while(num != 1){	//runs until num == 1 
+				if(num % 2 == 0)
+					num = num/2;
+				else
+					num = num*3 + 1;
+				
+				count++;
+				if (verbose) 
+					System.out.print(num + " ");
+			}
+			
+			if(verbose)
+				System.out.println("(" + count + ")");
+		}
+
+		System.out.println("Every one of the first " + upTo + " hailstone sequences reached 1.");
 	}
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,23 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+         //Get current hours and minutes and minutes to add
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int addMin = Integer.parseInt(args[1]);
+
+        //calculate new hour and minutes
+        int totalMin = (hours * 60) + minutes + addMin;  
+        int totalHour = totalMin / 60;
+        int newHour = totalHour % 24;
+        int newMin = totalMin - (totalHour * 60);
+
+        //show new hours and minutes in 24h clock
+        if(newHour < 10)
+            System.out.print("0");
+        System.out.print(newHour + ":");
+
+        if(newMin<10)
+            System.out.print("0");
+        System.out.print(newMin);
     }
 }
