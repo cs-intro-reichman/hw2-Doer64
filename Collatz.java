2ecd3c74228ad674146337522d61592a1baedb50
Dean Orion <60265116+Doer64@users.noreply.github.com>
2024-11-14 11:01:24 +0200

    Replaced while with do-while

--- Collatz.java
+++ Collatz.java
@@ -5,33 +5,25 @@ public class Collatz {
 		boolean verbose = args[1].equals("v");
 
 		for(int i = 1; i <= upTo; i++){
-			int count = 2;
+			int count = 1;
 			int num = i;
 			if (verbose)  //print the first number
 					System.out.print(num + " ");
 
-			if(num % 2 == 0) //runs for the first time in case upTo = 1
-				num = num/2;
-			else
-				num = num*3 + 1;
-			if (verbose) 
-				System.out.print(num + " ");
-
-			while(num != 1){	//runs until num == 1 
+			do{	//uses do-while incase seed is 1. runs until num == 1 
 				if(num % 2 == 0)
 					num = num/2;
 				else
 					num = num*3 + 1;
-				
 				count++;
 				if (verbose) 
 					System.out.print(num + " ");
-			}
+			}while(num != 1);
 			
 			if(verbose)
 				System.out.println("(" + count + ")");
 		}
 
-		System.out.println("Every one of the first " + upTo + " hailstone sequences reached 1.");
+		System.out.println("Every one of the first " + upTo + " hailstone sequences reached 1."); //summery line
 	}
 }
